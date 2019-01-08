6add84363cdd3fe991cb2c65790fe2275e8c765a
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2019-01-08 14:33:41 +0100

    startgame filters new game

--- app/Http/Controllers/WebInterface.php
+++ app/Http/Controllers/WebInterface.php
@@ -22,14 +22,38 @@ class WebInterface extends Controller
 
     public function startGame(Request $request)
     {
-        $game = new Game();
-        $game->status = 'pending';
-        $game->id_board = 1;
-        //$game->id_board = $request->board_id;
-        $game->save();
+        $boardId = 1;
+        $httpCode = 0;
+        $response = [];
 
-        event(new RaspberryRequestNewGame($game));
+        $game = Game::where('id_board', $boardId)
+            ->where('status', 'like', 'pending')
+            ->orWhere('status', 'like', 'running')
+            ->get();
 
-        return array('channel_id' => $game->id);
+        if (count($game) == 1) {
+            $game = $game[0];
+            $response["channel_id"] = $game->id;
+            $response["status"] = $game->status;
+            $httpCode = 200;
+            event(new RaspberryRequestNewGame($game));
+        } else if (count($game) == 0) {
+            $game = new Game();
+            $game->status = 'pending';
+            $game->id_board = $boardId;
+            $game->save();
+
+            $httpCode = 201;
+            $response["channel_id"] = $game->id;
+            $response["status"] = $game->status;
+            event(new RaspberryRequestNewGame($game));
+        } else {
+            $httpCode = 409;
+            $response = [
+                "Error" => "More than one game requested or running on the same board. Check your games and reset your board before trying again.",
+            ];
+        }
+
+        return response()->json($response, $httpCode);
     }
 }
