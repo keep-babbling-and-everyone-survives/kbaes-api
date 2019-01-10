ee27004a4ca230e79e34131d9e97a2efbcab06d8
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2019-01-10 17:37:37 +0100

    made the game creation routine

--- app/Http/Controllers/RaspberryInterface.php
+++ app/Http/Controllers/RaspberryInterface.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use App\Model\Game;
 use App\Events\Website\GameCreatedSuccess;
 use App\Events\Website\GameUpdate;
 use App\Events\Raspberry\GameUpdate as RaspberryGameUpdate;
+use App\Model\Game;
 use App\Model\Rule_Set;
+use App\Model\Board;
+use App\Model\Module;
 
 class RaspberryInterface extends Controller
 {
+    // Route : /api/game/confirm/{id}
     public function confirmGame(Request $req, $id)
     {
         try {
@@ -23,17 +26,55 @@ class RaspberryInterface extends Controller
             ], 404);
         }
 
+        if ($game->status !== "pending") {
+            return response()->json([
+                "error" => "You can't confirm a running game."
+            ], 409);
+        }
+
         $game->status = "running";
         $game->save();
 
         event(new GameCreatedSuccess($game));
 
-        $nextRuleset = $this->initRuleSet();
+        $this->initRuleSets($game);
+        $nextRuleset = $game->rulesets[0];
+
         event(new RaspberryGameUpdate($game, $nextRuleset));
-        event(new GameUpdate($game, $nextRuleset));
     }
 
-    private function initRuleSet() {
-        return new Rule_Set();
+    private function initRuleSets(Game $game) {
+        $nofmodules = 2; // $game->options[modules]
+
+        $rulesets = Rule_Set::All();
+        $availableRuleSets = [];
+        foreach($rulesets as $rs) {
+            $rsIsAvailable = false;
+            foreach($rs->modules as $module) {
+                foreach($module->boards as $board) {
+                    if ($board->id === $game->id_board)
+                        $rsIsAvailable = true;
+                }
+            }
+
+            if ($rsIsAvailable)
+                array_push($availableRuleSets, $rs);
+        }
+
+        unset($rulesets);
+
+        if (count($availableRuleSets) < $nofmodules) {
+            $nofmodules = count($availableRuleSets);
+        }
+
+        $chosenRuleSets = [];
+        for($i = 0; $i < $nofmodules; $i++) {
+            $generated = $availableRuleSets[rand(0, count($availableRuleSets))];
+            while (in_array($generated, $chosenRuleSets)) {
+                $generated = $availableRuleSets[rand(0, count($availableRuleSets))];
+            }
+            array_push($chosenRuleSets, $generated);
+            $game->rulesets()->attach($generated, ["solved" => false, "correct" => false,]);
+        }
     }
 }
